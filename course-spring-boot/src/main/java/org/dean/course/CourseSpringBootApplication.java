069302c7bf81ec8fdc0024adaa314c19a2cb5903
Dean <limeng4006@sina.com>
2020-05-31 23:46:51 +0800

    启动主要类

--- course-spring-boot/src/main/java/org/dean/course/CourseSpringBootApplication.java
+++ course-spring-boot/src/main/java/org/dean/course/CourseSpringBootApplication.java
@@ -3,6 +3,10 @@ package org.dean.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * springApplication.run-->ConfigurableApplicationContext-->ApplicationContext-->AbstractApplicationContext#refresh()
+ *
+ */
 @SpringBootApplication
 public class CourseSpringBootApplication {
 

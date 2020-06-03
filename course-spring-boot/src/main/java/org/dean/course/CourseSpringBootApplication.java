fa9c7af562e482835ae7e47bf2895a563edbd648
Dean <limeng4006@sina.com>
2020-06-03 23:04:41 +0800

    Spring boot 注释修改

--- course-spring-boot/src/main/java/org/dean/course/CourseSpringBootApplication.java
+++ course-spring-boot/src/main/java/org/dean/course/CourseSpringBootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * springApplication.run-->ConfigurableApplicationContext-->ApplicationContext-->AbstractApplicationContext#refresh()
+ * spring-boot入口
  *
  */
 @SpringBootApplication

9215d6452b61180b7258752773b3b0b3bccbf73b
Dean <limeng4006@sina.com>
2020-06-11 18:44:21 +0800

    Bootstrap 加载类

--- course-spring-boot/src/main/java/org/dean/course/CourseSpringBootApplication.java
+++ course-spring-boot/src/main/java/org/dean/course/CourseSpringBootApplication.java
@@ -3,6 +3,9 @@ package org.dean.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URL;
+import java.util.Arrays;
+
 /**
  * spring-boot入口
  *
@@ -11,6 +14,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseSpringBootApplication {
 
     public static void main(String[] args) {
+        URL[] urls=sun.misc.Launcher.getBootstrapClassPath().getURLs();
+
+        for (URL url : urls) {
+            System.out.println(url.toExternalForm());
+        }
         SpringApplication.run(CourseSpringBootApplication.class, args);
     }
 

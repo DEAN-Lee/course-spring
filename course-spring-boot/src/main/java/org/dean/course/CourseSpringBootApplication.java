690df49e79d7c57126da483661cd2cafa060232b
Dean <limeng4006@sina.com>
2020-06-11 23:22:52 +0800

    #2 md 文档启动流程-jvm 加载类

--- course-spring-boot/src/main/java/org/dean/course/CourseSpringBootApplication.java
+++ course-spring-boot/src/main/java/org/dean/course/CourseSpringBootApplication.java
@@ -8,17 +8,24 @@ import java.util.Arrays;
 
 /**
  * spring-boot入口
- *
  */
 @SpringBootApplication
 public class CourseSpringBootApplication {
 
     public static void main(String[] args) {
-        URL[] urls=sun.misc.Launcher.getBootstrapClassPath().getURLs();
+
+        System.out.println("bootstrapClassLoader" + System.getProperty("sun.boot.class.path"));
+        System.out.println("=================");
+        System.out.println("ExtClassLoader" + System.getProperty("java.ext.dirs"));
+        System.out.println("=================");
+        System.out.println("AppClassLoader" + System.getProperty("java.class.path"));
+
+        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
 
         for (URL url : urls) {
             System.out.println(url.toExternalForm());
         }
+
         SpringApplication.run(CourseSpringBootApplication.class, args);
     }
 
